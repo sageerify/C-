e39df9c22f5a1af779aba561281a884c480f8b76
Sageer Ahmed <160208153+sageerify@users.noreply.github.com>
2025-01-18 08:27:34 -0800

    Add files via upload

---
+++ FOP/Arthimatic operato.cpp
@@ -0,0 +1,10 @@
+//C++ program to demonstrate the arithmetic operatos
+#include <iostream>
+using namespace std;
+int main(){
+	int a=10, b=5;
+	//arthematic operators
+	cout<<"a+b="<<(a+b)<<endl;
+	cout<<"a-b="<<(a-b)<<endl;
+	return 0;
+}
---
+++ FOP/Arthimatic operator..cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=15, y=4;
+	cout<<"Sum:"<< (x+y)<< endl;
+	cout<<"Difference:" <<(x-y)<<endl;
+	cout<<"Product:" <<(x*y) <<endl;
+	cout<<"Quotient:"<<(x/y) <<endl;
+	cout<<"Remainder:"<<(x%y) <<endl;
+	return 0;
+}
